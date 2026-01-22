5c629b22ecce022d1910aa41c92779ed471fbefa
SAM SOPHAL2026 <sophalsam04@gmail.com>
2026-01-22 11:46:49 +0700

    Add User components with destructuring in App

---
+++ promps_destructuring.jsx
@@ -0,0 +1,14 @@
+// Promps destructuring & Function component in jsx
+
+function App(){
+    return (
+        <div>
+            <User name = "John" age = {21} role = "Developer"></User>
+            <User name = "Sophal" age = {18} role = "Software development"></User>
+            <User name = "Sophal" age = {19} role = "Graphic designer"></User>
+            <User name = "Virak Un" age = {18} role = "Mobile App development"></User>
+        </div>
+        
+        
+    )
+}
