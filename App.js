b07e0e7777a7126424d95f1d646f8fd7236b2aee
SAM SOPHAL2026 <sophalsam04@gmail.com>
2026-01-22 11:47:21 +0700

    Create App.js

---
+++ App.js
@@ -0,0 +1,10 @@
+import {Text, View, } from 'react-native';
+
+export default
+function App(){
+    return (
+        <View><Text>Hello world Welcome to Learning React native </Text></View>
+        
+    );
+}
+
