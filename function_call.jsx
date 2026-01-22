a2d3525b23ce9d65be104fa8cec77219ceca7e0c
SAM SOPHAL2026 <sophalsam04@gmail.com>
2026-01-22 11:45:30 +0700

    Add greeting and information display functions

---
+++ function_call.jsx
@@ -0,0 +1,16 @@
+function greet(user){
+    return 'Hello , ${user}';
+}
+
+<h2>{greet('sophal')}</h2>
+function test (age) {
+    return 'Age : ${age}';
+}
+<h2>{test(18)}</h2>
+
+function follow (address) {
+    return 'Place of Birth : ${address}';
+
+}
+
+<h2>{follow("Kandal Province")}</h2>
